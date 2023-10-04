59b45d492afa925b0f3315f672066d62331d8f31
Homvi <honvedoadamkj@gmail.com>
2023-10-04 21:06:56 +0200

    create quantity button

---
+++ app/components/IncreaseButton.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useState } from "react";
+
+const IncreaseButton = () => {
+  const [number, setNumber] = useState(1);
+
+  return (
+    <div className="counter-button rounded-lg">
+      <span
+        className="orange"
+        onClick={() => {
+          if (number > 1) {
+            setNumber((curr) => curr - 1);
+          }
+        }}
+      >
+        -
+      </span>
+      <span>{number}</span>
+      <span className="orange" onClick={() => setNumber((curr) => curr + 1)}>
+        +
+      </span>
+    </div>
+  );
+};
+
+export default IncreaseButton;
