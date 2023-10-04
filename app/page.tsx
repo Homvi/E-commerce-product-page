5aba5f1610462847233e0538def5f7f6ce13c71c
Homvi <honvedoadamkj@gmail.com>
2023-10-04 21:05:19 +0200

    add content instance

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Carousel from "./components/Carousel";
+import Content from "./components/Content";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-12">
+    <div className="h-screen p-12 flex">
       <Carousel />
+      <Content />
     </div>
   );
 }
