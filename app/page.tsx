c9c899e17562aabd98f3ae5834fa840d8b2fe5ea
Homvi <honvedoadamkj@gmail.com>
2023-10-07 10:22:28 +0200

    add Modal instance

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import Carousel from "./components/Carousel";
+import Carousel from "./components/carousel/Carousel";
 import Content from "./components/Content";
+import Modal from "./components/modal/Modal";
 
 export default function Home() {
   return (
     <div className="h-screen p-12 flex">
       <Carousel />
       <Content />
+      <Modal />
     </div>
   );
 }
