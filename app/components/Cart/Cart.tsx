a14415a60efe19127225d8546694b89e4c89ebd5
Homvi <honvedoadamkj@gmail.com>
2023-10-09 18:15:10 +0200

    handle delete items from cart

--- app/components/Cart/Cart.tsx
+++ app/components/Cart/Cart.tsx
@@ -3,14 +3,27 @@ import thumbnailOne from "../../assets/images/image-product-1-thumbnail.jpg";
 import deleteLogo from "../../assets/images/icon-delete.svg";
 import "./style.scss";
 import Image from "next/image";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { useDispatch } from "react-redux";
+import { setCart } from "../../redux/slices/cartSlice";
 
 const Cart = () => {
+  const dispatch = useDispatch();
+  const itemCount = useSelector((state: RootState) => state.cart);
+  if (itemCount === 0) {
+    return;
+  }
+
+  const handleSetCartValue = (value: number) => {
+    dispatch(setCart(value));
+  };
+
   return (
     <div id="cart">
       <div className="header">
         <h2>Cart</h2>
       </div>
-
       <div className="details">
         <div className="left">
           <div className="thumbnail">
@@ -19,12 +32,17 @@ const Cart = () => {
           <div className="text">
             <div className="name">Fall Limited Edition Sneakers</div>
             <div className="price">
-              $125.00 × 3 <span className="highlight">$375.00</span>
+              $125.00 × {itemCount}{" "}
+              <span className="highlight">${125 * itemCount} </span>
             </div>
           </div>
         </div>
         <div className="delete-container">
-          <Image src={deleteLogo} alt="delete" />
+          <Image
+            src={deleteLogo}
+            alt="delete"
+            onClick={() => handleSetCartValue(0)}
+          />
         </div>
       </div>
       <div className="button-container">
