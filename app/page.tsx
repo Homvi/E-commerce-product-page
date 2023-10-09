8a1d10cd4ace62e2e803ead426b111dec4ed6243
Homvi <honvedoadamkj@gmail.com>
2023-10-09 18:13:46 +0200

    add navbar into provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
+"use client";
+
+import { Provider } from "react-redux";
 import Carousel from "./components/carousel/Carousel";
 import Content from "./components/Content";
 import Modal from "./components/modal/Modal";
+import store from "./redux/store";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="h-screen p-12 flex">
-      <Carousel />
-      <Content />
-      <Modal />
-    </div>
+    <Provider store={store}>
+      <Navbar />
+      <div className="h-screen p-12 flex">
+        <Carousel />
+        <Content />
+        <Modal />
+      </div>
+    </Provider>
   );
 }
