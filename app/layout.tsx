6d123e53ff5cc35991c4d0dbbd612627d97db90a
Homvi <honvedoadamkj@gmail.com>
2023-10-09 18:13:32 +0200

    move navbar to page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import "./globals.scss";
 import type { Metadata } from "next";
 import { Kumbh_Sans } from "next/font/google";
-import Navbar from "./components/Navbar";
-
 const kumbhSans = Kumbh_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,10 +16,7 @@ export default function RootLayout({
   return (
     <html className={kumbhSans.className} lang="en">
       <body>
-        <section className="max-w-[1250px] mx-auto">
-          <Navbar />
-          {children}
-        </section>
+        <section className="max-w-[1250px] mx-auto">{children}</section>
       </body>
     </html>
   );
