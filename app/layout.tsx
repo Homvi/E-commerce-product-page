766c9aaadc1fd204a3be61dbe3ed79636a559408
Homvi <honvedoadamkj@gmail.com>
2023-10-10 11:23:54 +0200

    move navbar to layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,24 @@
 import "./globals.scss";
 import type { Metadata } from "next";
 import { Kumbh_Sans } from "next/font/google";
+import Navbar from "./components/Navbar";
 const kumbhSans = Kumbh_Sans({ subsets: ["latin"] });
+import { Providers } from "@/lib/providers";
 
 export const metadata: Metadata = {
   title: "Sneakers",
   description: "Sneakers shop",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout(props: React.PropsWithChildren) {
   return (
-    <html className={kumbhSans.className} lang="en">
-      <body>
-        <section className="max-w-[1250px] mx-auto">{children}</section>
-      </body>
-    </html>
+    <Providers>
+      <html className={kumbhSans.className} lang="en">
+        <body>
+          <Navbar />
+          <section className="max-w-[1250px] mx-auto">{props.children}</section>
+        </body>
+      </html>
+    </Providers>
   );
 }
