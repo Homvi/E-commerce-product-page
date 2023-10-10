0cfc1028afd3261703c4f57d057ef2e0f076c54a
Homvi <honvedoadamkj@gmail.com>
2023-10-10 11:24:28 +0200

    import navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,13 @@
-"use client";
-
-import { Provider } from "react-redux";
 import Carousel from "./components/carousel/Carousel";
 import Content from "./components/Content";
 import Modal from "./components/modal/Modal";
-import store from "./redux/store";
-import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <Provider store={store}>
-      <Navbar />
       <div className="h-screen p-12 flex">
         <Carousel />
         <Content />
         <Modal />
       </div>
-    </Provider>
   );
 }
