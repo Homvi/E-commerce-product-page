fb9ee06b32d57f7efd726d8e0d4905ae181b6649
Homvi <honvedoadamkj@gmail.com>
2023-10-11 10:39:04 +0200

    create

---
+++ app/constants/images.ts
@@ -0,0 +1,29 @@
+import imageOneBig from "../assets/images/image-product-1.jpg";
+import imageTwoBig from "../assets/images/image-product-2.jpg";
+import imageThreeBig from "../assets/images/image-product-3.jpg";
+import imageFourBig from "../assets/images/image-product-4.jpg";
+
+import imageFourSmall from "../assets/images/image-product-4-thumbnail.jpg";
+import imageThreeSmall from "../assets/images/image-product-3-thumbnail.jpg";
+import imageTwoSmall from "../assets/images/image-product-2-thumbnail.jpg";
+import imageOneSmall from "../assets/images/image-product-1-thumbnail.jpg";
+
+export const images = [
+    {
+      normal: imageOneBig,
+      thumbnail: imageOneSmall,
+    },
+    {
+      normal: imageTwoBig,
+      thumbnail: imageTwoSmall,
+    },
+    {
+      normal: imageThreeBig,
+      thumbnail: imageThreeSmall,
+    },
+    {
+      normal: imageFourBig,
+      thumbnail: imageFourSmall,
+    },
+  ];
+
