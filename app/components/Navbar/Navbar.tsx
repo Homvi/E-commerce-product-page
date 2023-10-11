80a1a527fbe759db313817e1b6d7d4e9312c064a
Homvi <honvedoadamkj@gmail.com>
2023-10-11 11:56:11 +0200

    add burger icon on mobile widht

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import logo from "../../assets/images/logo.svg";
 import cart from "../../assets/images/icon-cart.svg";
 import avatar from "../../assets/images/image-avatar.png";
 import Cart from "../Cart/Cart";
+import burger from "../../assets/images/icon-menu.svg";
 import "./style.scss";
 import { useSelector, selectCart } from "@/lib/redux";
 
@@ -15,12 +16,15 @@ const Navbar = () => {
   const [isCartOpen, setIsCartOpen] = useState(false);
 
   return (
-    <nav className="sticky top-0 border-b-2 h-28 flex justify-between bg-white">
+    <nav className="sticky max-w-[90%] mx-auto top-0 md:border-b-2 h-14 md:h-28 flex justify-between bg-white">
       <div id="left" className="flex items-center">
-        <div id="logo" className="mr-14 flex items-center">
+        <div id="burger" className="mr-3 flex items-center md:hidden">
+          <Image src={burger} alt="logo" className="opacity-90 " />
+        </div>
+        <div id="logo" className="m-1 md:m-0 md:mr-14 flex items-center">
           <Image src={logo} alt="logo" width={130} className="opacity-90" />
         </div>
-        <ul className="flex gap-7  h-full items-center">
+        <ul className="md:flex gap-7  h-full items-center hidden ">
           <li>
             <a href="#">Collections</a>
           </li>
@@ -40,7 +44,7 @@ const Navbar = () => {
       </div>
       <div id="right" className="flex items-center h-full">
         <div
-          className="cart-logo-container flex items-center relative h-full mx-7 cursor-pointer"
+          className="cart-logo-container flex items-center relative  mx-7 cursor-pointer"
           onClick={() => setIsCartOpen((curr) => !curr)}
         >
           <div id="cart-counter-bedge">{count}</div>
@@ -51,11 +55,11 @@ const Navbar = () => {
             src={avatar}
             alt="avatar"
             width={50}
-            className="opacity-90 avatar-image"
+            className="opacity-90 avatar-image w-7 md:w-[50px]"
           />
         </div>
       </div>
-    { isCartOpen &&  <Cart />}  
+      {isCartOpen && <Cart />}
     </nav>
   );
 };
