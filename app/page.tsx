d67849faa304db77955df6f238b17bd016483f08
Homvi <honvedoadamkj@gmail.com>
2023-10-11 10:37:59 +0200

    render modal conditionally

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import Carousel from "./components/carousel/Carousel";
 import Content from "./components/Content";
 import Modal from "./components/modal/Modal";
+import { useSelector, selectImage } from "@/lib/redux";
 
 export default function Home() {
+  const selectedImage = useSelector(selectImage);
   return (
-      <div className="h-screen p-12 flex">
-        <Carousel />
-        <Content />
-        <Modal />
-      </div>
+    <div className="h-screen p-12 flex">
+      <Carousel />
+      <Content />
+      {selectedImage !== null && <Modal />}
+    </div>
   );
 }
