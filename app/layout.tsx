54cbe0d6dd24d53c508b397771bbff3382818549
Homvi <honvedoadamkj@gmail.com>
2023-10-11 18:17:21 +0200

    add favicon in head

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Kumbh_Sans } from "next/font/google";
 import Navbar from "./components/Navbar/Navbar";
 const kumbhSans = Kumbh_Sans({ subsets: ["latin"] });
 import { Providers } from "@/lib/providers";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Sneakers",
@@ -14,6 +15,9 @@ export default function RootLayout(props: React.PropsWithChildren) {
   return (
     <Providers>
       <html className={kumbhSans.className} lang="en">
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
         <body className="overflow-x-hidden">
           <section className="max-w-[1250px] mx-auto">
             <Navbar />
